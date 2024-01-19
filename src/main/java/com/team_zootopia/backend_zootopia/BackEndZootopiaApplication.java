c4c595bcdfad70d0e9bd7f088772b3498a5abd98
Juan Munguia <carlosmunguia23145@gmail.com>
2024-01-19 08:44:17 +0100

    Repository configurate

---
+++ src/main/java/com/team_zootopia/backend_zootopia/BackEndZootopiaApplication.java
@@ -0,0 +1,13 @@
+package com.team_zootopia.backend_zootopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndZootopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndZootopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team_zootopia/backend_zootopia/BackEndZootopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team_zootopia.backend_zootopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndZootopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
