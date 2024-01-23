192597a45aa046d3a77cc5b39a511c1cc05a4537
Stella Maris González Robert <stegonyrob@yahoo.es>
2024-01-23 13:05:14 +0100

    add animals

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -3,3 +3,26 @@ INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_
 INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Cocodrilo', 'Reptil', 'macho', 'America Central y del Sur', 'Cocodylus', 'Cocodrilo', './src/assets/img/cocodrilo.jpg');
 INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Comadreja', 'Mustelidae', 'macho', 'Europa, Asia y Africa', 'Mustela', 'Comadreja', './src/assets/img/comadreja2.jpg');
 INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Conejo', 'Leporidae', 'macho', 'America del Norte', 'Oryctolagus', 'Conejo', './src/assets/img/conejo.jpg');
+
+
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Coyote', 'Canidae', 'macho', 'America del Norte', 'Canis', 'Coyote', './src/assets/img/coyote.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Huron', 'Felidae', 'Hembra', 'Norteamerica', 'Lynx', 'Huron', './src/assets/img/huron.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Iguana', 'Reptil', 'Hembra', 'America Central y del Sur', 'date', 'Iguanidae', 'Iguana', './src/assets/img/iguana.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'León', 'Felidae', 'Hembra', 'Africa, Asia y América.', 'Panthera', 'León', './src/assets/img/leon.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Leopardo', 'Felidae', 'Hembra', 'Africa y Asia', 'Panthera', 'Leopardo', './src/assets/img/leopardo.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Liebre', 'Leporidae', 'macho', 'Mundial', 'Lepus', 'Liebre', './src/assets/img/liebre.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Linces', 'Felidae', 'macho', 'Eurasia', 'Lynx', 'Linces', './src/assets/img/lince.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Nutria', 'Myocastoridae', 'macho', 'Sudamerica', 'date', 'Myocastor', 'Nutria', './src/assets/img/nutria.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Panda', 'Mustelidae', 'macho', 'Asia', 'Ailuropoda', 'Panda', './src/assets/img/panda.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Pantera', 'Felidae', 'macho', 'Africa, Asia y America.jpg', 'Panthera', 'Pantera', './src/assets/img/pantera.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Puma', 'Felidae', 'macho', 'America', 'Puma', 'Puma', './src/assets/img/puma.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Rata Almizclera', 'Mustelidae', 'macho', 'America del Norte', 'Mus', 'Rata Almizclera', './src/assets/img/rataalmizclera.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Serpiente Anaconda', 'Reptil', 'femenino', 'Sudamerica', 'Boidae', 'Anaconda', './src/assets/img/serpienteanaconda.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Serpeinte Cascabel', 'Reptil', 'macho', 'America del Sur', 'Crotalus', 'Cascabel', './src/assets/img/serpientecascabel.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Tejon', 'Sciuridae', 'Hembra', 'Norteamerica', 'Tamias', 'Tejon', './src/assets/img/tejon.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Tigre', 'Felidae', 'Hembra', 'Asia', 'Panthera', 'Tigre', './src/assets/img/tigre.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Tigre', 'Felidae', 'Hembra', 'India', 'Panthera', 'Tigre de Bengala', './src/assets/img/tigredebengala.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Tortuga', 'Reptil', 'Hembra', 'Mundo', 'Chelonia', 'Tortuga Marina', './src/assets/img/tortugamarina.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Yacare', 'Reptil', 'Hembra', 'America del Sur', 'Caiman', 'Yacare', './src/assets/img/yacare.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Zorro', 'Canidae', 'Hembra', 'Europa y Asia', 'Vulpes', 'Zorro', './src/assets/img/zorro.jpg');
+INSERT INTO animals (id_animal, name, family, gener, origin, type, title, image_path) VALUES (default, 'Zorro de las Nieves', 'Canidae', 'Macho', 'Artico', 'Vulpes', 'Zorro Nival', './src/assets/img/zorrodelasnieves.jpg');
